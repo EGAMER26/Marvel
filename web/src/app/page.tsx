46e0c3abfab9d99ecc012fa0a63c7d1392ff6a37
EGAMER26 <evandrogomes542@gmail.com>
2023-07-27 14:55:01 -0300

    feito o input para pesquisar os herois, não funcional ainda

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className='bg-white h-screen'>
+    <main className='bg-white h-screen flex flex-col items-center'>
       <header>
       <div className='w-auto flex flex-col justify-center text-center h-auto'>
         <div className='flex justify-center'>
@@ -12,6 +12,10 @@ export default function Home() {
         <p className='mt-2'>Mergulhe no domínio Deslumbrante de todos os personagens clássicos que você ama - e aqueles que você descobrirá em breve!</p>
       </div>
       </header>
+      <section className='flex gap-5 w-2/3 mt-20 h-16 text-red-700 items-center'>
+      <Image width={34} height={24} className='h-7' src="/assets/busca/Lupa/Shape@2x.png" alt="lupa" />
+        <input type="text" className='h-auto w-screen placeholder-red-500 pl-6 focus:outline-red-600 outline-none rounded-xl' placeholder='Procure por heróis' name="search" id="" />
+      </section>
     </main>
 
   )
