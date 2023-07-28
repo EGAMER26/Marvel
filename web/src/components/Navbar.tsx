134f24a6b5b9363c8f7f37a5b86258ffe5d7634b
EGAMER26 <evandrogomes542@gmail.com>
2023-07-28 19:18:41 -0300

    o input de pesquisa esta redirecilnado ao endereço correto

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -1,14 +1,32 @@
+'use client';
+
 import Image from 'next/image'
-import {FC} from 'react'
+import {useRouter} from 'next/navigation';
+import {FC, KeyboardEvent, useState} from 'react';
 
-interface NavbarProps {
 
-}
+const Navbar: FC = () => {
+  const router = useRouter();
+  const [querySearch, setquerySearch] = useState<string>('');
 
-const Navbar: FC<NavbarProps> = ({}) => {
+  const handleSearch = (event: KeyboardEvent<HTMLInputElement>) => {
+    if(event.key === 'Enter' && querySearch.trim() !== '') {
+        setquerySearch('');
+        router.push(`/search?query=${querySearch}`);
+
+        
+    }
+  }
+
   return <section className='flex gap-5 w-2/3 relative h-16 items-center '>
   <Image width={34} height={24} className='h-6 w-6 absolute left-6' src="/assets/busca/Lupa/Shape@2x.png" alt="lupa" />
-    <input type="text" className='h-16 w-screen placeholder-red-500 bg-red-200 pl-20 focus:outline-red-600 outline-none rounded-full' placeholder='Procure por heróis' name="search" id="" />
+    <input type="text" className='h-16 w-screen placeholder-red-500 bg-red-200 pl-20 focus:outline-red-600 outline-none rounded-full' 
+    placeholder='Procure por heróis' 
+    name="search" 
+    value={querySearch}
+    onChange={(e => setquerySearch(e.target.value))}
+    onKeyDown={handleSearch}
+    />
   </section>
 }
 
