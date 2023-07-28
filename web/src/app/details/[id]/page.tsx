b4b5a89c51f109ed19dff20b7c2b3220044d76df
EGAMER26 <evandrogomes542@gmail.com>
2023-07-28 12:05:15 -0300

    feito os cards identos aos de referencia, ainda desenvolvendo a details page

--- web/src/app/details/[id]/page.tsx
+++ web/src/app/details/[id]/page.tsx
@@ -1,4 +1,5 @@
 import {detailCharacter} from '@/utils/api';
+import Image from 'next/image';
 import { FC } from "react";
 
 interface CharacterPageProps {
@@ -11,7 +12,18 @@ interface CharacterPageProps {
 const CharacterPage: FC<CharacterPageProps> = async ({ params }) => {
   const { id } = params;
   const character = await detailCharacter(id);
-  return <div>CharacterPage</div>
+  return <div className='container'>
+    <div className="aboutContent">
+      <h2>${character.results[0].name}</h2>
+      <p>${character.results[0].description}</p>
+    </div>
+    <Image 
+    src={`${character.results[0].thumbnail.path}.${character.results[0].thumbnail.extension}`}
+    alt={character.results[0].name}
+    width={500}
+    height={500}
+    />
+  </div>
 }
 
 export default CharacterPage;
--- web/src/components/CharacterCard.tsx
+++ web/src/components/CharacterCard.tsx
@@ -10,22 +10,24 @@ interface characterCardProps {
 
 const CharacterCard: FC<characterCardProps> = ({character}) => {
   console.log(character); 
-  return <Link href={`details/${character.id}`}>
-  <div className="w-full max-w-sm h-64 bg-red-400 rounded-xl">
-  <figure>
-    <Image 
-    src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
-    alt={character.name}
-    width={500}
-    height={500}
-    />
-  </figure>
-  <div className="flex justify-between w-auto ">
+  return <div className="w-full max-w-sm h-64 mb-8 hover:scale-105 hover:shadow-black hover:shadow-2xl overflow-hidden ease-in duration-200">
+    <Link href={`details/${character.id}`}>
+  <div className='overflow-hidden h-48 border-b-4 border-solid border-red-600'>
+    <figure>
+      <Image
+      src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+      alt={character.name}
+      width={500}
+      height={500}
+      />
+    </figure>
+  </div>
+  <div className="flex justify-between w-auto h-20 font-bold items-center ">
     <p>{character.name}</p>
     <Image width={20} height={15}src="/assets/icones/heart/Path Copy 2@3x.png" alt="" />
   </div>
-</div>
 </Link>
+</div>
 }
 
 export default CharacterCard
