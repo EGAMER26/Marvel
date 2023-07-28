be96d99d66960d94c8a0499c0a92bc36e3b3f887
EGAMER26 <evandrogomes542@gmail.com>
2023-07-28 19:58:26 -0300

    a pesquisa esta sendo feita, e esta retornando valores via console, começando a aplicar nos cards

--- web/src/app/search/page.tsx
+++ web/src/app/search/page.tsx
@@ -1,12 +1,36 @@
+'use client'
 import Navbar from '@/components/Navbar';
+import { Character } from '@/types/marvels';
+import { searCharacters } from '@/utils/api';
 import Image from 'next/image';
-import { FC } from 'react'
+import { useSearchParams } from 'next/navigation';
+import { FC, useEffect, useState } from 'react'
 
-interface SearchPageProps {
   
-}
 
-const SearchPage: FC<SearchPageProps> = ({}) => {
+const SearchPage: FC= () => {
+  const searchParams = useSearchParams();
+  const querySearch = searchParams.get('query');
+  const [characters, setCharacters] = useState<Character[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  console.log(characters);
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const data = await searCharacters(querySearch);
+        setCharacters(data.results);
+      } catch(error) {
+        console.error(error);
+      }
+      setIsLoading(false);
+    };
+    if(querySearch) {
+      fetchData();
+    }
+  }, [querySearch]);
   return <main>
     <nav className='flex gap-24 pl-20 items-center'>
     <Image height={0} width={400} className='h-20' src="/assets/logo/Group@3x.png" alt='logo'/>
